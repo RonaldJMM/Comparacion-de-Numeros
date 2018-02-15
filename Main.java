0cc6c1d661b1e20369d4bd6c406d1a3980c6376d
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-15 13:09:57 -0500

    Desiciones o Comparadores
    Comparaciones o desiciones de los dos numeros para ser evaluados.

--- Main.java
+++ Main.java
@@ -7,11 +7,26 @@ public class Main{
 		System.out.println("\n\n\t\tEjercicio No.1");
 		System.out.println("");
 		System.out.println("");
-		
+
+		//variables y numeros a comparar
 		float num1 = 40;
 		float num2 = 20;
 
-		
+		//desiciones para la comparacion de los numeros.
+		if(num1<num2){
+
+			
+		}
+
+		if(num1>num2){
+
+			
+		}
+
+		if(num1==num2){
+
+			
+		}
 		
 	}
 
