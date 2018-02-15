99100d8c3908acd1d548ccddafc73ec124bc662e
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-15 13:13:06 -0500

    Mensajes de las Desiciones
    Se crean los mensajes de cada comparador para mostrar su respectiva salida o respuesta evaluada.

--- Main.java
+++ Main.java
@@ -12,19 +12,22 @@ public class Main{
 		float num1 = 40;
 		float num2 = 20;
 
-		//desiciones para la comparacion de los numeros.
+		//desiciones para la comparacion de los numeros y sus mensajes.
 		if(num1<num2){
 
-			
+			System.out.println("El numero "+(num1)+" es menor que el numero "+(num2));
+			System.out.println("");
 		}
 
 		if(num1>num2){
-
+			System.out.println("El numero "+(num1)+" es mayor que el numero "+(num2));
+			System.out.println("");
 			
 		}
 
 		if(num1==num2){
-
+			System.out.println("El numero "+(num1)+" es igual que el numero "+(num2));
+			System.out.println("");
 			
 		}
 		
