a79ce6508fa358059010bc8cbdab3a935b475494
Ronald Jefrey Moreno Mora <morenoronald7@gmail.com>
2018-02-15 13:04:56 -0500

    Declaracion de Variables Y Numeros.
    Se crean las variables de los dos numeros a comparar y se definen como float.

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+	
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("Comparacion de Numeros");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio No.1");
+		System.out.println("");
+		System.out.println("");
+		
+		float num1 = 40;
+		float num2 = 20;
+
+		
+		
+	}
+
+}
